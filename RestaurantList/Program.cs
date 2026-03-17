1a597dddb10bb46286bf966ce536885a02eab822
Tim <timrealdev@outlook.com>
2026-03-17 15:18:42 +1000

    feat: add sql db and configure db connection string

--- RestaurantList/Program.cs
+++ RestaurantList/Program.cs
@@ -1,7 +1,14 @@
+using RestaurantList.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<RestaurantListContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString"))
+);
 
 var app = builder.Build();
 
