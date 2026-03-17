cb4c0fa191f98135a932240e0e4361a0f0e1fabd
Tim <timrealdev@outlook.com>
2026-03-17 10:30:46 +1000

    feat: add context folder and file

---
+++ RestaurantList/Data/RestaurantListContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RestaurantList.Data
+{
+    // RestaurantListContext class inherits from DbContext
+    public class RestaurantListContext : DbContext
+    {
+        // Constructor that takes DbContextOptions and passes it to the base class constructor
+        public RestaurantListContext(DbContextOptions<RestaurantListContext> options) : base(options)
+        {
+        }
+    }
+}
