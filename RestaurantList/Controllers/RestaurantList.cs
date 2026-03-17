165eda875b6c358bf7bf45c722b951f82a0ba727
Tim <timrealdev@outlook.com>
2026-03-17 16:03:29 +1000

    feat: add restaurant list controller and view

---
+++ RestaurantList/Controllers/RestaurantList.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestaurantList.Data;
+using RestaurantList.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace RestaurantList.Controllers
+{
+    public class RestaurantList : Controller
+    {
+        private readonly RestaurantListContext _context;
+        public RestaurantList(RestaurantListContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Restaurants.ToListAsync());
+        }
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+    }
+}
