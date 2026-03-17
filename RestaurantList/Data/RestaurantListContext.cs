6e25a1dfdd47aea3858bb5945e010157a111d741
Tim <timrealdev@outlook.com>
2026-03-17 11:05:23 +1000

    feat: define keys and bridge between code and db

--- RestaurantList/Data/RestaurantListContext.cs
+++ RestaurantList/Data/RestaurantListContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using RestaurantList.Models;
 
 namespace RestaurantList.Data
 {
@@ -9,5 +10,27 @@ namespace RestaurantList.Data
         public RestaurantListContext(DbContextOptions<RestaurantListContext> options) : base(options)
         {
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // defines composite key for the RestaurantDish entity/ helper model using RestaurantId and DishId
+            modelBuilder.Entity<RestaurantDish>().HasKey(rd => new { rd.RestaurantId, rd.DishId });
+
+            modelBuilder.Entity<RestaurantDish>()
+                .HasOne(r => r.Restaurant)
+                .WithMany(rd => rd.RestaurantDishes)
+                .HasForeignKey(r => r.RestaurantId);
+
+            modelBuilder.Entity<RestaurantDish>()
+               .HasOne(d => d.Dish)
+               .WithMany(rd => rd.RestaurantDishes)
+               .HasForeignKey(d => d.DishId);
+
+            base.OnModelCreating(modelBuilder);
+        }
+        // each DbSet corresponds to a table in the database
+        // DbSet acts as a bridge between our code and database, allowing us to perform CRUD operations on the entities
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<Dish> Dishes { get; set; }
+        public DbSet<RestaurantDish> RestaurantDishes { get; set; }
     }
 }
